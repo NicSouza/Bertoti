f0830b4c06af56d3b41bdd7168a24c6bf8c3d387
Nicole Souza <108769169+NicSouza@users.noreply.github.com>
2023-10-02 21:16:16 -0300

    Back biblioteca

---
+++ Lab III/Biblioteca/src/Biblioteca.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.List;
+
+import java.util.LikedList;
+
+public class Biblioteca {
+
+	private List<Livro> livros = new LinkedList<Livro>();
+	
+	public void cadastrarLivro(Livro livro){
+		livros.add(livro);
+	}
+	public List<Livro> buscarLivroPorEspecificação(Especificacao espec){
+		List<Livro> livrosEncontrados = new LinkedList<Livro>();
+		
+	}
+}
---
+++ Lab III/Biblioteca/src/Livro.java
@@ -0,0 +1,25 @@
+
+public class Livro {
+	
+	private String titulo;
+	private Especificacao espec;
+	
+	
+	
+	public Livro(String titulo, Especificacao espec) {
+		this.titulo = titulo;
+		this.espec = espec;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Especificacao getEspec() {
+		return espec;
+	}
+	public void setEspec(Especificacao espec) {
+		this.espec = espec;
+	}
+}
