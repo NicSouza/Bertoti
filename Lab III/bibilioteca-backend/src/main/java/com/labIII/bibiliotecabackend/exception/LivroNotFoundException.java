08a7ee5b74a7d5509c9271377c958147b12e3cea
Nicole Souza <nicolemadeleines@gmail.com>
2023-11-17 19:54:17 -0300

    att

--- Lab III/bibilioteca-backend/src/main/java/com/labIII/bibiliotecabackend/exception/LivroNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.labIII.bibiliotecabackend.exception;
-
-public class LivroNotFoundException extends RuntimeException{
-    public LivroNotFoundException(Long id){
-        super("Não foi possível encontrar o livro com o id:"+id);
-    }
-}
--- Lab III/bibilioteca-backend/src/main/java/com/labIII/bibiliotecabackend/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.labIII.bibiliotecabackend.repository;
-
-import com.labIII.bibiliotecabackend.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
